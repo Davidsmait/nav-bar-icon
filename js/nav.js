f39dc5648e40c40cfa408db2199e41d0b9bca81f
Davidsmait <deivod_halo@hotmail.com>
2018-10-29 18:11:28 -0600

    commit 2

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,21 @@
-let bar_icon = document.getElementById("bar");
-let count = 0;
+let bar_icon = document.getElementById("bar")
+let nav = document.getElementById('nav-tag')
+let content_page = document.getElementById('content-page')
 
 
 bar_icon.addEventListener("click",e=>{
-
-        
-        if(e.isTrusted && count === 0){
-            document.getElementById('nav').style.display = 'block';
-            count++;
-        }else if(e.isTrusted && count === 1){
-            document.getElementById('nav').style.display = 'none';
-            count--;
-        }
+    
+    nav.style.left = '0';
+    nav.style.boxShadow = ' 2px 0 30px #222';
+    content_page.style.marginLeft = '220px';
+    content_page.style.transition = '.5s';
+      
 })
 
+
+document.getElementById('close-nav').addEventListener('click', () =>{
+    nav.style.left = '-300px';
+    nav.style.boxShadow = ' none';
+    content_page.style.marginLeft = '0';
+
+})
